0e8d2a4dec46a57a9f37911f64161805a19b9517
tkdev0 <bjson11223@gmail.com>
2020-11-11 19:07:25 -0500

    Updating venom to work like magi's bane, with increased damage and mana cost

--- rtklua/Accepted/Spells/mage/venom.lua
+++ rtklua/Accepted/Spells/mage/venom.lua
@@ -1,47 +1,113 @@
-local _maxDamage = 2000
-
 venom_mage = {
-	cast = function(player, target)
-		local magicCost = 60
+	cast = function(player)
+		local castable = {}
+		local maxX = 8
+		local maxY = 7
+		local magicCost = player.baseMagic * 0.05
 
-		if (not player:canCast(1, 1, 0)) then
-			return
+		if player.magic < magicCost then
+			return;
 		end
 
-		if (player.magic < magicCost) then
-			player:sendMinitext("Not enough mana.")
-			return
+		if player.x < 8 then
+			maxX = maxX + (8 - player.x)
 		end
 
-		if (target.blType == BL_PC) then
-			player:sendMinitext("It doesn't work.")
-			return
+		if player.y < 8 then
+			maxY = maxY + (8 - player.y)
 		end
 
-		if target:checkIfCast(venoms) then
-			player:sendMinitext("Another spell of this type is already cast.")
-			return
+		if player.x > getMapXMax(player.m) - 8 then
+			maxX = maxX + 9 - (getMapXMax(player.m) - player.x)
 		end
 
-		player:sendAction(6, 35)
-		player:playSound(24)
-		player.magic = player.magic - magicCost
-		player:sendMinitext("You cast Venom.")
-		player:sendStatus()
+		if player.y > getMapYMax(player.m) - 8 then
+			maxY = maxY + 9 - (getMapYMax(player.m) - player.y)
+		end
 
-		if (target.blType == BL_MOB) then
-			target:setDuration("venom_mage", 1 + math.random(3000, 30000))
-			target:sendAnimation(1)
-			target.attacker = player.ID
+		for x = 0, maxX do
+			for y = 0, maxY do
+				local mob = player:getObjectsInCell(
+					player.m,
+					player.x + x,
+					player.y + y,
+					BL_MOB
+				)
+
+				if #mob > 0 then
+					if not mob[1]:checkIfCast(venoms) then
+						table.insert(castable, mob[1])
+						break
+					end
+				end
+
+				local mob = player:getObjectsInCell(
+					player.m,
+					player.x + x,
+					player.y - y,
+					BL_MOB
+				)
+
+				if #mob > 0 then
+					if not mob[1]:checkIfCast(venoms) then
+						table.insert(castable, mob[1])
+						break
+					end
+				end
+
+				local mob = player:getObjectsInCell(
+					player.m,
+					player.x - x,
+					player.y + y,
+					BL_MOB
+				)
+
+				if #mob > 0 then
+					if not mob[1]:checkIfCast(venoms) then
+						table.insert(castable, mob[1])
+						break
+					end
+				end
+
+				local mob = player:getObjectsInCell(
+					player.m,
+					player.x - x,
+					player.y - y,
+					BL_MOB
+				)
+
+				if #mob > 0 then
+					if not mob[1]:checkIfCast(venoms) then
+						table.insert(castable, mob[1])
+						break
+					end
+				end
+			end
+
+			if #castable > 0 then
+				break
+			end
+		end
+
+		if #castable > 0 then
+			player:sendAction(6, 35)
+			player.magic = player.magic - magicCost
+			player:sendStatus()
+			player:playSound(24)
+			player:sendMinitext("You cast Venom.")
+	
+			if (castable[1].blType == BL_MOB) then
+				castable[1]:setDuration("venom_mage", 1 + math.random(3000, 30000))
+				castable[1]:sendAnimation(1)
+				castable[1].attacker = player.ID
+			end
+        else
+            player:sendMinitext("No targets available.")
 		end
 	end,
 
 	while_cast_1500 = function(target)
-		local damage = target.baseHealth *.01
-
-		if (damage > _maxDamage) then
-			damage = _maxDamage
-		end
+		local damage = target.baseHealth *.015
 
 		if (damage < 1) then
 			damage = 1
@@ -50,7 +116,7 @@ venom_mage = {
 		if (target.blType == BL_PC) then
 			local player = Player(target.attacker)
 			if (player:canPK(target)) then
-				target:sendAnimation(1)
+				target:sendAnimation(84)
 				target.attacker = player.ID
 				if (target.health > damage) then
 					target:removeHealthExtend(damage, 0, 0, 0, 0, 0)
@@ -59,8 +125,7 @@ venom_mage = {
 				end
 			end
 		else
-			target:sendAnimation(1)
-
+			target:sendAnimation(84)
 			if (target.health > damage) then
 				target:removeHealthExtend(damage, 0, 0, 0, 0, 0)
 			else
@@ -82,55 +147,126 @@ venom_mage = {
 			0
 		}
 		local itemAmounts = {10, 3, 1, 500}
-		local description = "Poisons target for random amount of time. Does 1000 damage a second."
+		local description = "Poisons target for random amount of time. Does 1.5% damage a second."
 		return level, items, itemAmounts, description
 	end
 }
 
 spirits_leech_mage = {
-	cast = function(player, target)
-		local magicCost = 60
+	cast = function(player)
+		local castable = {}
+		local maxX = 8
+		local maxY = 7
+		local magicCost = player.baseMana * 0.05
 
-		if (not player:canCast(1, 1, 0)) then
-			return
+		if player.magic < magicCost then
+			return;
 		end
 
-		if (player.magic < magicCost) then
-			player:sendMinitext("Not enough mana.")
-			return
-		end
-		if (target.blType == BL_PC) then
-			player:sendMinitext("It doesn't work.")
-			return
+		if player.x < 8 then
+			maxX = maxX + (8 - player.x)
 		end
 
-		if target:checkIfCast(venoms) then
-			player:sendMinitext("Another spell of this type is already cast.")
-			return
+		if player.y < 8 then
+			maxY = maxY + (8 - player.y)
 		end
 
-		player:sendAction(6, 35)
-		player:playSound(24)
-		player.magic = player.magic - magicCost
-		player:sendMinitext("Spirit's leech.")
-		player:sendStatus()
-		if (target.blType == BL_MOB) then
-			target:setDuration(
-				"spirits_leech_mage",
-				1 + math.random(1500, 30000)
-			)
-			target:sendAnimation(84)
-			target.attacker = player.ID
+		if player.x > getMapXMax(player.m) - 8 then
+			maxX = maxX + 9 - (getMapXMax(player.m) - player.x)
+		end
+
+		if player.y > getMapYMax(player.m) - 8 then
+			maxY = maxY + 9 - (getMapYMax(player.m) - player.y)
+		end
+
+		for x = 0, maxX do
+			for y = 0, maxY do
+				local mob = player:getObjectsInCell(
+					player.m,
+					player.x + x,
+					player.y + y,
+					BL_MOB
+				)
+
+				if #mob > 0 then
+					if not mob[1]:checkIfCast(venoms) then
+						table.insert(castable, mob[1])
+						break
+					end
+				end
+
+				local mob = player:getObjectsInCell(
+					player.m,
+					player.x + x,
+					player.y - y,
+					BL_MOB
+				)
+
+				if #mob > 0 then
+					if not mob[1]:checkIfCast(venoms) then
+						table.insert(castable, mob[1])
+						break
+					end
+				end
+
+				local mob = player:getObjectsInCell(
+					player.m,
+					player.x - x,
+					player.y + y,
+					BL_MOB
+				)
+
+				if #mob > 0 then
+					if not mob[1]:checkIfCast(venoms) then
+						table.insert(castable, mob[1])
+						break
+					end
+				end
+
+				local mob = player:getObjectsInCell(
+					player.m,
+					player.x - x,
+					player.y - y,
+					BL_MOB
+				)
+
+				if #mob > 0 then
+					if not mob[1]:checkIfCast(venoms) then
+						table.insert(castable, mob[1])
+						break
+					end
+				end
+			end
+
+			if #castable > 0 then
+				break
+			end
+		end
+
+		if #castable > 0 then
+			player:sendAction(6, 35)
+			player.magic = player.magic - magicCost
+			player:sendStatus()
+			player:playSound(24)
+			player:sendMinitext("You cast Spirit's Leech.")
+	
+			if (mob[1].blType == BL_MOB) then
+				mob[1]:setDuration("spirits_leech_mage", 1 + math.random(3000, 30000))
+				mob[1]:sendAnimation(1)
+				mob[1].attacker = player.ID
+			end
+        else
+            player:sendMinitext("No targets available.")
 		end
 	end,
+
 	while_cast_1500 = function(target)
-		local damage = target.baseHealth *.01
-		if (damage > 1000) then
-			damage = 1000
-		end
+		local damage = target.baseHealth *.015
+
 		if (damage < 1) then
 			damage = 1
 		end
+
 		if (target.blType == BL_PC) then
 			local player = Player(target.attacker)
 			if (player:canPK(target)) then
@@ -166,50 +302,126 @@ spirits_leech_mage = {
 			0
 		}
 		local itemAmounts = {10, 3, 1, 500}
-		local description = "Poisons target for random amount of time. Does 1000 damage a second."
+		local description = "Poisons target for random amount of time. Does 1.5% damage a second."
 		return level, items, itemAmounts, description
 	end
 }
 
 snake_bite_mage = {
-	cast = function(player, target)
-		local magicCost = 60
-		if (not player:canCast(1, 1, 0)) then
-			return
-		end
-		if (player.magic < magicCost) then
-			player:sendMinitext("Not enough mana.")
-			return
-		end
-		if (target.blType == BL_PC) then
-			player:sendMinitext("It doesn't work.")
-			return
+	cast = function(player)
+		local castable = {}
+		local maxX = 8
+		local maxY = 7
+		local magicCost = player.baseMana * 0.05
+
+		if player.magic < magicCost then
+			return;
 		end
 
-		if target:checkIfCast(venoms) then
-			player:sendMinitext("Another spell of this type is already cast.")
-			return
+		if player.x < 8 then
+			maxX = maxX + (8 - player.x)
 		end
 
-		player:sendAction(6, 35)
-		player:playSound(24)
-		player.magic = player.magic - magicCost
-		player:sendMinitext("You cast Snake bite.")
-		player:sendStatus()
-		if (target.blType == BL_MOB) then
-			target:setDuration("snake_bite_mage", 1 + math.random(1500, 30000))
-			target:sendAnimation(84)
-			target.attacker = player.ID
+		if player.y < 8 then
+			maxY = maxY + (8 - player.y)
+		end
+
+		if player.x > getMapXMax(player.m) - 8 then
+			maxX = maxX + 9 - (getMapXMax(player.m) - player.x)
+		end
+
+		if player.y > getMapYMax(player.m) - 8 then
+			maxY = maxY + 9 - (getMapYMax(player.m) - player.y)
+		end
+
+		for x = 0, maxX do
+			for y = 0, maxY do
+				local mob = player:getObjectsInCell(
+					player.m,
+					player.x + x,
+					player.y + y,
+					BL_MOB
+				)
+
+				if #mob > 0 then
+					if not mob[1]:checkIfCast(venoms) then
+						table.insert(castable, mob[1])
+						break
+					end
+				end
+
+				local mob = player:getObjectsInCell(
+					player.m,
+					player.x + x,
+					player.y - y,
+					BL_MOB
+				)
+
+				if #mob > 0 then
+					if not mob[1]:checkIfCast(venoms) then
+						table.insert(castable, mob[1])
+						break
+					end
+				end
+
+				local mob = player:getObjectsInCell(
+					player.m,
+					player.x - x,
+					player.y + y,
+					BL_MOB
+				)
+
+				if #mob > 0 then
+					if not mob[1]:checkIfCast(venoms) then
+						table.insert(castable, mob[1])
+						break
+					end
+				end
+
+				local mob = player:getObjectsInCell(
+					player.m,
+					player.x - x,
+					player.y - y,
+					BL_MOB
+				)
+
+				if #mob > 0 then
+					if not mob[1]:checkIfCast(venoms) then
+						table.insert(castable, mob[1])
+						break
+					end
+				end
+			end
+
+			if #castable > 0 then
+				break
+			end
+		end
+
+		if #castable > 0 then
+			player:sendAction(6, 35)
+			player.magic = player.magic - magicCost
+			player:sendStatus()
+			player:playSound(24)
+			player:sendMinitext("You cast Snake Bite.")
+	
+			if (mob[1].blType == BL_MOB) then
+				mob[1]:setDuration("snake_bite_mage", 1 + math.random(3000, 30000))
+				mob[1]:sendAnimation(1)
+				mob[1].attacker = player.ID
+			end
+        else
+            player:sendMinitext("No targets available.")
 		end
 	end,
+
 	while_cast_1500 = function(target)
-		local damage = target.baseHealth *.01
-		if (damage > 1000) then
-			damage = 1000
-		end
+		local damage = target.baseHealth *.015
+
 		if (damage < 1) then
 			damage = 1
 		end
+
 		if (target.blType == BL_PC) then
 			local player = Player(target.attacker)
 			if (player:canPK(target)) then
@@ -245,50 +457,126 @@ snake_bite_mage = {
 			0
 		}
 		local itemAmounts = {10, 3, 1, 500}
-		local description = "Poisons target for random amount of time. Does 1000 damage a second."
+		local description = "Poisons target for random amount of time. Does 1.5% damage a second."
 		return level, items, itemAmounts, description
 	end
 }
 
 corruption_mage = {
-	cast = function(player, target)
-		local magicCost = 60
-		if (not player:canCast(1, 1, 0)) then
-			return
-		end
-		if (player.magic < magicCost) then
-			player:sendMinitext("Not enough mana.")
-			return
-		end
-		if (target.blType == BL_PC) then
-			player:sendMinitext("It doesn't work.")
-			return
+	cast = function(player)
+		local castable = {}
+		local maxX = 8
+		local maxY = 7
+		local magicCost = player.baseMana * 0.05
+
+		if player.magic < magicCost then
+			return;
 		end
 
-		if target:checkIfCast(venoms) then
-			player:sendMinitext("Another spell of this type is already cast.")
-			return
+		if player.x < 8 then
+			maxX = maxX + (8 - player.x)
 		end
 
-		player:sendAction(6, 35)
-		player:playSound(24)
-		player.magic = player.magic - magicCost
-		player:sendMinitext("You cast Corruption.")
-		player:sendStatus()
-		if (target.blType == BL_MOB) then
-			target:setDuration("corruption_mage", 1 + math.random(1500, 30000))
-			target:sendAnimation(84)
-			target.attacker = player.ID
+		if player.y < 8 then
+			maxY = maxY + (8 - player.y)
+		end
+
+		if player.x > getMapXMax(player.m) - 8 then
+			maxX = maxX + 9 - (getMapXMax(player.m) - player.x)
+		end
+
+		if player.y > getMapYMax(player.m) - 8 then
+			maxY = maxY + 9 - (getMapYMax(player.m) - player.y)
+		end
+
+		for x = 0, maxX do
+			for y = 0, maxY do
+				local mob = player:getObjectsInCell(
+					player.m,
+					player.x + x,
+					player.y + y,
+					BL_MOB
+				)
+
+				if #mob > 0 then
+					if not mob[1]:checkIfCast(venoms) then
+						table.insert(castable, mob[1])
+						break
+					end
+				end
+
+				local mob = player:getObjectsInCell(
+					player.m,
+					player.x + x,
+					player.y - y,
+					BL_MOB
+				)
+
+				if #mob > 0 then
+					if not mob[1]:checkIfCast(venoms) then
+						table.insert(castable, mob[1])
+						break
+					end
+				end
+
+				local mob = player:getObjectsInCell(
+					player.m,
+					player.x - x,
+					player.y + y,
+					BL_MOB
+				)
+
+				if #mob > 0 then
+					if not mob[1]:checkIfCast(venoms) then
+						table.insert(castable, mob[1])
+						break
+					end
+				end
+
+				local mob = player:getObjectsInCell(
+					player.m,
+					player.x - x,
+					player.y - y,
+					BL_MOB
+				)
+
+				if #mob > 0 then
+					if not mob[1]:checkIfCast(venoms) then
+						table.insert(castable, mob[1])
+						break
+					end
+				end
+			end
+
+			if #castable > 0 then
+				break
+			end
+		end
+
+		if #castable > 0 then
+			player:sendAction(6, 35)
+			player.magic = player.magic - magicCost
+			player:sendStatus()
+			player:playSound(24)
+			player:sendMinitext("You cast Corruption.")
+	
+			if (mob[1].blType == BL_MOB) then
+				mob[1]:setDuration("corruption_mage", 1 + math.random(3000, 30000))
+				mob[1]:sendAnimation(1)
+				mob[1].attacker = player.ID
+			end
+        else
+            player:sendMinitext("No targets available.")
 		end
 	end,
+
 	while_cast_1500 = function(target)
-		local damage = target.baseHealth *.01
-		if (damage > 1000) then
-			damage = 1000
-		end
+		local damage = target.baseHealth *.015
+
 		if (damage < 1) then
 			damage = 1
 		end
+
 		if (target.blType == BL_PC) then
 			local player = Player(target.attacker)
 			if (player:canPK(target)) then
@@ -324,7 +612,7 @@ corruption_mage = {
 			0
 		}
 		local itemAmounts = {10, 3, 1, 500}
-		local description = "Poisons target for random amount of time. Does 1000 damage a second."
+		local description = "Poisons target for random amount of time. Does 1.5% damage a second."
 		return level, items, itemAmounts, description
 	end
 }
